2c65e1506d43e2880cc0d9025f1fec08b6bbe5f7
Aanas Aamer <anasamer056@gmail.com>
2023-12-07 22:19:42 +0200

    Implement invalid input style

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const root = document.documentElement;
 // Intialize the theme color to light 
 root.className = "light";
 
+document.querySelector("#first-name").setCustomValidity("Invalid field.");
+document.querySelector("#first-name::after").textContent = "sadasd"
