c9c5d3f5fbf8ef4d1e7db6dc8cc76e55d72b43d3
Aanas Aamer <anasamer056@gmail.com>
2023-12-09 21:05:46 +0200

    Implment custom validation logic for month field

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const root = document.documentElement;
 // Intialize the theme color to light 
 root.className = "light";
 
+
+// Month Input Validation 
+const monthInput = document.querySelector("#month");
+const monthError = document.querySelector("#month ~ div");
+
+monthInput.addEventListener("input", ()=>{
+    const month = parseInt(monthInput.value);
+    if (month < 1 || month > 12) {
+        monthInput.setCustomValidity("Invalid month");
+    
+    } else if (isNaN(month)) {
+        monthInput.setCustomValidity("Invalid input");
+        monthError.dataset.help = "Invalid input";
+    }
+    else {
+        monthInput.setCustomValidity("");
+        monthError.dataset.help = "Invalid month";
+    }
+})
