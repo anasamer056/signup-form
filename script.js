eb009d37850bc84320afcec3285abbc6ed01e65f
Aanas Aamer <anasamer056@gmail.com>
2023-12-09 21:15:57 +0200

    Implement custom validation logic for day and year

--- script.js
+++ script.js
@@ -21,3 +21,42 @@ monthInput.addEventListener("input", ()=>{
         monthError.dataset.help = "Invalid month";
     }
 })
+
+// Day Input Validation 
+const dayInput = document.querySelector("#day");
+const dayError = document.querySelector("#day ~ div");
+
+dayInput.addEventListener("input", ()=>{
+    const day = parseInt(dayInput.value);
+    if (day < 1 || day > 31) {
+        dayInput.setCustomValidity("Invalid day");
+    
+    } else if (isNaN(day)) {
+        dayInput.setCustomValidity("Invalid input");
+        dayError.dataset.help = "Invalid input";
+    }
+    else {
+        dayInput.setCustomValidity("");
+        dayError.dataset.help = "Invalid day";
+    }
+})
+
+// Year Input Validation 
+const yearInput = document.querySelector("#year");
+const yearError = document.querySelector("#year ~ div");
+
+yearInput.addEventListener("input", ()=>{
+    const year = parseInt(yearInput.value);
+    if (year < 1900 || year > new Date().getFullYear()) {
+        yearInput.setCustomValidity("Invalid year");
+    
+        yearError.dataset.help = "Invalid year"
+    } else if (isNaN(year)) {
+        yearInput.setCustomValidity("Invalid input");
+        yearError.dataset.help = "Invalid input";
+    }
+    else {
+        yearInput.setCustomValidity("");
+        yearError.dataset.help = "Invalid year";
+    }
+})
